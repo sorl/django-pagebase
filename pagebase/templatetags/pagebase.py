8850a75c92f5bea304dabc5ce804e44cc9c83f6c
Mikko Hellsing <mikko@aino.se>
2011-02-02 04:10:24 +0100

    forking those painful functions

--- pagebase/templatetags/pagebase.py
+++
@@ -1,9 +0,0 @@
-from django.template import Library
-from django.contrib.admin.templatetags.admin_list import result_list
-
-
-register = Library()
-
-
-result_list = register.inclusion_tag("pagebase/admin/change_list_results.html")(result_list)
-
