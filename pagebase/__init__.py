b454c8e46ea2284295f3259febfcc4119e44b130
Mikko Hellsing <mikko@aino.se>
2011-02-04 14:17:27 +0100

    ->base

--- pagebase/__init__.py
+++ pagebase/__init__.py
@@ -1,5 +1,5 @@
 from pagebase.admin import PageBaseAdmin
 from pagebase.helpers import register
-from pagebase.models.abstract import PageBase, PageBaseMeta
+from pagebase.models.base import PageBase, PageBaseMeta
 from pagebase.models.fields import AutoSlugField
 
